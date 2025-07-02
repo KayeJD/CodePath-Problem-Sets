f6502ee28b86a61506ec3c5030939ea8cdeb90f5
Karryl Dumalag <kjdumalag@gmail.com>
2025-07-01 21:31:22 -0700

    implement standard set 2, q1-7 in oop_and_linked_lists1.py

--- oop_and_linked_lists1.py
+++ oop_and_linked_lists1.py
@@ -1,4 +1,4 @@
-# Standard set 1 \
+# Standard set 1 
 
 class Villager:
     def __init__(self, name, species, catchphrase, personality):
@@ -203,11 +203,227 @@ Evaluate the time and space complexity of your solution. Define your variables a
 def find_closest_nft_values(nft_values, budget):
     pass
 
-# Check
-nft_values = [3.5, 5.4, 7.2, 9.0, 10.5]
-nft_values_2 = [2.0, 4.5, 6.3, 7.8, 12.1]
-nft_values_3 = [1.0, 2.5, 4.0, 6.0, 9.0]
+# # Check
+# nft_values = [3.5, 5.4, 7.2, 9.0, 10.5]
+# nft_values_2 = [2.0, 4.5, 6.3, 7.8, 12.1]
+# nft_values_3 = [1.0, 2.5, 4.0, 6.0, 9.0]
 
-print(find_closest_nft_values(nft_values, 8.0))
-print(find_closest_nft_values(nft_values_2, 6.5))
-print(find_closest_nft_values(nft_values_3, 3.0))
+# print(find_closest_nft_values(nft_values, 8.0))
+# print(find_closest_nft_values(nft_values_2, 6.5))
+# print(find_closest_nft_values(nft_values_3, 3.0))
+
+# Standard set v2
+
+# Q1 ================================================================================================
+'''
+Step 1: Copy the following code into your IDE.
+
+Step 2: Instantiate an instance of the class Player and store it in a variable named player_one.
+
+The Player object should have the character "Yoshi" and the kart "Super Blooper".
+'''
+
+class Player():
+    def __init__(self, character, kart, opponent=None):
+        self.character = character
+        self.kart = kart
+        self.items = []
+        self.ahead = opponent
+
+    def get_player(self):
+        return f"{self.character} driving the {self.kart}"
+    
+    def set_character(self, name):
+        # Figure out valid parameters
+        if name in ("Mario", "Luigi", "Peach", "Yoshi", "Toad", "Wario", "Donkey Kong", "Bowser"):
+            print("Character Updated")
+            self.character = name
+        else:
+            print("Invalid Character")
+
+    def add_item(self, item_name):
+        if item_name in ("banana", "green shell", "red shell", "bob-omb", "super star", "lightning", "bullet bill"):
+            self.items.append(item_name)
+
+    def print_inventory(self):
+        count={}
+
+        if not self.items:
+            print("Inventory empty")
+        else:
+            for items in self.items:
+                if items in count:
+                    count[items]+=1
+                else:
+                    count[items]=1
+                
+            print(count)
+
+        a="Inventory: "
+
+        for item, num in count.items():
+            a += f"{item}: {num}, "
+
+        print(a)
+
+
+
+
+player_one = Player("Yoshi", "Super Blooper")
+
+# Check
+player_one.character
+player_one.kart
+player_one.items
+
+# Q1 ================================================================================================
+'''
+Step 1: Using the Player class from Problem 1, add the following get_player() method to your existing code:
+
+def get_player(self):
+    return f"{self.character} driving the {self.kart}"
+Step 2: Create a second instance of Player in a variable named player_two.
+
+The Player object created should have character "Bowser" and kart "Pirahna Prowler".
+Step 3: Use the method get_player() below to print out "Match: Yoshi driving the Super Blooper vs Bowser driving the Pirahna Prowler".
+'''
+
+player_two = Player("Bowser", "Pirahna Prowler")
+
+# Q2 ================================================================================================
+
+print(player_two.character, player_two.kart, player_two.items)
+
+# Q3 ================================================================================================
+
+player_one.kart = "Dolphin Dasher"
+print(player_one.get_player())
+
+# Q4 ================================================================================================
+
+player_three = Player("Donkey Kong", "Standard Kart")
+
+player_three.set_character("Peach")
+print(player_three.character)
+player_three.set_character("Baby Peach")
+print(player_three.character)
+
+# Q5 ================================================================================================
+'''
+Players can pick up special items as they race.
+
+Update the Player class with a new method add_item() that takes in one parameter, item_name.
+
+The method should validate the item_name.
+
+If the item is valid, add item_name to the player’s items attribute.
+The method does not need to return any values.
+item_name is valid if it has one of the following values: "banana", "green shell", "red shell", "bob-omb", "super star", "lightning", "bullet bill".
+'''
+
+player_one = Player("Yoshi", "Dolphin Dasher")
+
+print(player_one.items)
+
+player_one.add_item("red shell")
+print(player_one.items)
+
+player_one.add_item("super star")
+print(player_one.items)
+
+player_one.add_item("super smash")
+print(player_one.items)
+
+# Q6 ================================================================================================
+'''
+Update the Player class with a method print_inventory() that accepts no parameters except for self.
+
+The method should print the name and quantity of each item in a player’s items list.
+
+If the player has no items, the function should print "Inventory empty".
+class Player():
+    # ... methods from previous problems
+    
+    def print_inventory(self):
+        pass
+'''
+
+player_one = Player("Yoshi", "Super Blooper")
+player_one.items = ["banana", "bob-omb", "banana", "super star"]
+player_two = Player("Peach", "Dolphin Dasher")
+
+player_one.print_inventory()
+player_two.print_inventory()
+
+# Q7 ================================================================================================
+'''
+Given a list race_results of Player objects where the first player in the list came first in the race, second player in the list came second, etc., write a function print_results() that prints the players in place.
+'''
+
+def print_results(race_results):
+    # create a for loop to access the values in race_results i, player 
+        # 
+
+    for i, player in enumerate(race_results):
+        print(f"{i + 1}. {player.character}")
+
+# test
+peach = Player("Peach", "Daytripper")
+mario = Player("Mario", "Standard Kart M")
+luigi = Player("Luigi", "Super Blooper")
+race_one = [peach, mario, luigi]
+
+print_results(race_one)
+# Output:
+# 1. Peach
+# 2. Mario
+# 3. Luigi
+
+# Q8 ================================================================================================
+
+def get_rank(my_player):
+    # create a array for order .pop(1, )
+    rank = 1
+
+    current_player = my_player
+
+
+    while current_player.ahead != None:
+        rank += 1
+        current_player = current_player.ahead
+
+    return rank   
+
+peach = Player("Peach", "Daytripper")
+mario = Player("Mario", "Standard Kart M", peach)
+luigi = Player("Luigi", "Super Blooper", mario)
+
+print(get_rank(luigi))
+print(get_rank(peach))
+print(get_rank(mario))
+
+# Q9 ================================================================================================
+'''
+Problem 9: Tom and Jerry
+A linked list is a new data type that, similar to a normal list or array, allows us to store pieces of data sequentially. The difference between a linked list and a normal list lies in how each element is stored in a computer’s memory.
+
+In a normal list, individual elements of the list are stored in adjacent memory locations according to the order they appear in the list. If we know where the first element of the list is stored, it’s really easy to find any other element in the list.
+
+In a linked list, the individual elements called nodes are not stored in sequential memory locations. Each node may be stored in an unrelated memory location. To connect nodes together into a sequential list, each node stores a reference or pointer to the next node in the list.
+
+Using the provided Node class below, create a linked list cat -> mouse where the instance cat has value "Tom" which points to the instance mouse that has value "Jerry".
+'''
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+mouse = Node("Jerry")
+
+cat = Node("Tom", mouse)
+
+print(cat.value)
+print(cat.next)
+print(cat.next.value)
+print(mouse.value)
+print(mouse.next)
